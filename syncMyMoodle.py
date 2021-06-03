c4992a287c48307a938c2d8774598ce96b1aa5af
Lukas Liesenberg <lukas.liesenberg@rwth-aachen.de>
2021-06-03 20:38:34 +0200

    fixed multiple downloads

--- syncMyMoodle.py
+++ syncMyMoodle.py
@@ -545,6 +545,10 @@ class SyncMyMoodle:
 	def downloadQuiz(self, node):
 		path = self.get_sanitized_node_path(node.parent)
 		os.makedirs(path, exist_ok=True)
+
+		if os.path.exists(os.path.join(path, f"{node.name}.pdf")):
+			return True
+
 		quiz_res = bs(self.session.get(node.url).text,features="html.parser")
 
 		# i need to hide the left nav element because its obscuring the quiz in the resulting pdf
